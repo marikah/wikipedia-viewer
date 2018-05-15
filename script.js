697ff2ef275aa99de145f97f6ef6056c159989d1
Marika Helttula <marika.helttula@gmail.com>
2018-05-15 11:56:28 +0300

    add Wikipedia API functionality

--- script.js
+++ script.js
@@ -1,41 +1,34 @@
 function searchWikipedia(){
-
   let userInput = document.getElementById('keyword');
   let keyword = userInput.value;
-  console.log(keyword);
+  let searchURL = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
+  let requestURL = searchURL + keyword;
 
-  // Get placeholder JSON for testing
-  var requestURL = 'https://jsonplaceholder.typicode.com/posts';
-  var request = new XMLHttpRequest();
-  request.open('GET', requestURL);
-  request.responseType = 'json';
-  request.send();
-  request.onload = function (){
-    var data = request.response;
-    createResults(data);
-  }
+  $.ajax({
+    url: requestURL,
+    dataType: "jsonp",
+    success: function (data){
+      createResults(data);
+  }});
+}
 
-};
-
-// Create elements for displaying results
 function createResults(jsonObj){
   for(var i = 0 ; i < 10; i++){
     var element = document.getElementById('results');
 
     var newDiv = document.createElement('div');
     newDiv.className = 'entry';
-    //newDiv.id = 'entry-' + i;
 
     var newH1 = document.createElement('h2');
-    var newTitleNode = document.createTextNode(jsonObj[i].title);
+    var newTitleNode = document.createTextNode(jsonObj[1][i]);
     newH1.appendChild(newTitleNode);
 
     var newP = document.createElement('p');
-    var newTextNode = document.createTextNode(jsonObj[i].body);
+    var newTextNode = document.createTextNode(jsonObj[2][i]);
     newP.appendChild(newTextNode);
 
     element.appendChild(newDiv);
     newDiv.appendChild(newH1);
     newH1.appendChild(newP);
   }
-};
+}
